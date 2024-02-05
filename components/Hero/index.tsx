b88048de410dc0ed7e00b3bf7ce51824434ae1d2
Syed Ali Hassan <syedalihassan6651@gmail.com>
2024-02-05 16:39:47 +0500

    Minor styling changes.

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -7,16 +7,16 @@ const Hero = () => {
     <>
       <section
         id="home"
-        className="dark:bg-[#0a0c3b] relative z-10 overflow-hidden bg-white pb-16 pt-[120px] md:pb-[120px] md:pt-[150px] xl:pb-[160px] xl:pt-[180px] 2xl:pb-[200px] 2xl:pt-[210px]"
+        className="dark:bg-[#0a0c3b] z-10 overflow-hidden bg-white"
       >
-        <div className="container">
-          <div className="-mx-4 flex flex-wrap">
+        <div className="container pb-16 pt-[120px] md:pb-[120px] md:pt-[150px] xl:pb-[160px] xl:pt-[180px] 2xl:pb-[200px] 2xl:pt-[210px] relative">
+          <div className="-mx-4 flex flex-wrap relative z-10">
             <div className="w-full px-4">
               <div
                 className="wow fadeInUp mx-auto max-w-[800px] flex flex-col justify-center"
                 data-wow-delay=".2s"
               >
-                <h1 className="mb-5 text-3xl xs:text-4xl font-bold leading-tight text-gray-dark dark:text-white sm:text-5xl sm:leading-tight md:text-7xl md:leading-tight text-left uppercase">
+                <h1 className="mb-5 text-3xl 2xs:text-4xl font-bold leading-tight text-gray-dark dark:text-white sm:text-5xl sm:leading-tight md:text-7xl md:leading-tight text-left uppercase">
                   <p className="text-primary/90">
                     <ReactTypingEffect
                       typingDelay={200}
@@ -34,13 +34,13 @@ const Hero = () => {
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-7 sm:space-y-0">
                   <Link
                     href="/contact"
-                    className="rounded-md bg-primary px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-transparent hover:text-primary border-[3px] border-transparent hover:border-primary"
+                    className="rounded-md bg-primary px-5 py-3 xs:px-8 xs:py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-transparent hover:text-primary border-[3px] border-transparent hover:border-primary"
                   >
                     Get in Touch
                   </Link>
                   <Link
                     href="/shop"
-                    className="inline-block rounded-md bg-transparent hover:bg-[#fed203] px-8 py-4 text-base font-semibold text-dark duration-300 ease-in-out dark:bg-white/10 dark:text-white border-[3px] border-yellow hover:text-white"
+                    className="inline-block rounded-md bg-transparent hover:bg-[#fed203] px-5 py-3 xs:px-8 xs:py-4 text-base font-semibold text-dark duration-300 ease-in-out dark:bg-white/10 dark:text-white border-[3px] border-yellow hover:text-white"
                   >
                     View Products
                   </Link>
@@ -48,13 +48,13 @@ const Hero = () => {
               </div>
             </div>
           </div>
-        </div>
-        <div className="absolute -right-10 sm:right-0 top-24 sm:top-14 lg:top-8 z-[-1]">
-          <img src="/images/spidey-bot.png" className="w-[16rem] sm:w-[25rem] lg:w-[35rem]" />
-        </div>
+          <div className="absolute -right-20 top-24 sm:top-14 lg:top-8">
+            <img src="/images/spidey-bot.png" className="w-[16rem] sm:w-[25rem] lg:w-[30rem] xl:w-[35rem]" />
+          </div>
 
-        <div className="absolute -bottom-10 -left-8 z-[-1] opacity-95">
-          <img src="/images/robuild-package.png" className="hidden md:block w-[15rem] lg:w-[25rem]" />
+          <div className="absolute -bottom-10 -left-28 opacity-75 sm:opacity-95">
+            <img src="/images/robuild-package.png" className="w-[15rem] md:w-[20rem] xl:w-[25rem]" />
+          </div>
         </div>
       </section>
     </>
