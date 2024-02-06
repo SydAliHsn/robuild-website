b8de1ca88f2f47e89f095832340694465a0e5e2f
Syed Ali Hassan <syedalihassan6651@gmail.com>
2024-02-06 21:24:27 +0500

    Change links.

--- components/Header/menuData.tsx
+++ components/Header/menuData.tsx
@@ -1,27 +1,27 @@
 import { Menu } from "@/types/menu";
 
 const menuData: Menu[] = [
-  {
-    id: 1,
-    title: "Home",
-    path: "/",
-    newTab: false,
-  },
+  // {
+  //   id: 1,
+  //   title: "Home",
+  //   path: "/",
+  //   newTab: false,
+  // },
   {
     id: 2,
     title: "About",
     path: "/about",
     newTab: false,
   },
-  // {
-  //   id: 33,
-  //   title: "Blog",
-  //   path: "/blog",
-  //   newTab: false,
-  // },
+  {
+    id: 5,
+    title: "Shop",
+    path: "/shop",
+    newTab: false,
+  },
   {
     id: 3,
-    title: "Support",
+    title: "Contact",
     path: "/contact",
     newTab: false,
   },
