c8a5a5abe29e8d0060d730f990959c40a3734882
Syed Ali Hassan <syedalihassan6651@gmail.com>
2024-02-06 21:25:00 +0500

    Add background.

--- components/Common/Breadcrumb.tsx
+++ components/Common/Breadcrumb.tsx
@@ -9,26 +9,26 @@ const Breadcrumb = ({
 }) => {
   return (
     <>
-      <section className="relative z-10 overflow-hidden pt-28 lg:pt-[150px]">
-        <div className="container">
+      <section className="relative z-10 overflow-hidden pt-20 lg:pt-[118px] bg-secondary dark:bg-primary-dark">
+        <div className="container bg-[url(/images/square.svg)] bg-repeat-round pt-8">
           <div className="-mx-4 flex flex-wrap items-center">
             <div className="w-full px-4 md:w-8/12 lg:w-7/12">
-              <div className="mb-8 max-w-[570px] md:mb-0 lg:mb-12">
-                <h1 className="uppercase mb-5 text-2xl font-bold text-black dark:text-white sm:text-3xl">
+              <div className="mb-4 max-w-[570px] md:mb-0 lg:mb-12">
+                <h1 className="uppercase mb-5 text-2xl font-bold text-primary dark:text-white sm:text-3xl">
                   {pageName}
                 </h1>
-                <p className="text-base font-medium leading-relaxed text-body-color">
+                <p className="text-base font-medium leading-relaxed text-white dark:text-body-color">
                   {description}
                 </p>
               </div>
             </div>
-            <div className="w-full px-4 md:w-4/12 lg:w-5/12">
+            <div className="w-full px-4 mb-4 md:w-4/12 lg:w-5/12">
               <div className="text-end">
                 <ul className="flex items-center md:justify-end">
                   <li className="flex items-center">
                     <Link
                       href="/"
-                      className="pr-1 text-base font-medium text-body-color hover:text-primary"
+                      className="pr-1 text-base font-medium text-white dark:text-body-color hover:text-primary"
                     >
                       Home
                     </Link>
@@ -53,7 +53,7 @@ const Breadcrumb = ({
               xmlns="http://www.w3.org/2000/svg"
             >
               <path
-                opacity="0.1"
+                opacity="0.2"
                 d="M286.5 0.5L-14.5 254.5V69.5L286.5 0.5Z"
                 fill="url(#paint0_linear_111:578)"
               />
@@ -66,8 +66,8 @@ const Breadcrumb = ({
                   y2="-97.1485"
                   gradientUnits="userSpaceOnUse"
                 >
-                  <stop stopColor="#4A6CF7" />
-                  <stop offset="1" stopColor="#4A6CF7" stopOpacity="0" />
+                  <stop stopColor="#074199" />
+                  <stop offset="1" stopColor="#074199" stopOpacity="0" />
                 </linearGradient>
               </defs>
             </svg>
@@ -81,12 +81,12 @@ const Breadcrumb = ({
               xmlns="http://www.w3.org/2000/svg"
             >
               <path
-                opacity="0.1"
+                opacity="0.2"
                 d="M669.125 257.002L345.875 31.9983L524.571 -15.8832L669.125 257.002Z"
                 fill="url(#paint0_linear_0:1)"
               />
               <path
-                opacity="0.1"
+                opacity="0.2"
                 d="M0.0716344 182.78L101.988 -15.0769L142.154 81.4093L0.0716344 182.78Z"
                 fill="url(#paint1_linear_0:1)"
               />
@@ -99,8 +99,8 @@ const Breadcrumb = ({
                   y2="37.0429"
                   gradientUnits="userSpaceOnUse"
                 >
-                  <stop stopColor="#4A6CF7" />
-                  <stop offset="1" stopColor="#4A6CF7" stopOpacity="0" />
+                  <stop stopColor="#074199" />
+                  <stop offset="1" stopColor="#074199" stopOpacity="0" />
                 </linearGradient>
                 <linearGradient
                   id="paint1_linear_0:1"
@@ -110,8 +110,8 @@ const Breadcrumb = ({
                   y2="32.3398"
                   gradientUnits="userSpaceOnUse"
                 >
-                  <stop stopColor="#4A6CF7" />
-                  <stop offset="1" stopColor="#4A6CF7" stopOpacity="0" />
+                  <stop stopColor="#074199" />
+                  <stop offset="1" stopColor="#074199" stopOpacity="0" />
                 </linearGradient>
               </defs>
             </svg>
