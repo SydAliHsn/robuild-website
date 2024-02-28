1c1ab47370b09c8ac3883c41b04db5ddde9a0f8f
Syed Ali Hassan <syedalihassan6651@gmail.com>
2024-02-29 00:31:45 +0500

    Make some changes for auto loading screen on each page.

--- components/Common/LoadingScreen.tsx
+++ components/Common/LoadingScreen.tsx
@@ -1,23 +1,37 @@
 'use client';
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Loading from "./LoadingGears"
 
-const LoadingScreen = ({ loading }: { loading: boolean }) => {
+const LoadingScreen = ({ loading }: { loading?: boolean }) => {
+    const [visible, setVisible] = useState(true);
 
     useEffect(() => {
-        if (loading)
-            document.body.classList.add('!overflow-hidden');
+        window.scrollTo(0, 0);
 
-        else
-            document.body.classList.remove('!overflow-hidden');
+        if (loading === undefined) {
+            document.querySelector('html')?.classList.add('!overflow-hidden');
+            setTimeout(() => {
+                document.querySelector('html')?.classList.remove('!overflow-hidden');
+                setVisible(false);
+            }, 850);
+        } else {
+            if (loading)
+                document.body.classList.add('!overflow-hidden');
+            else {
+                setVisible(false);
+                document.body.classList.remove('!overflow-hidden');
+            }
 
-        return () => {
-            document.body.classList.remove('!overflow-hidden');
-        };
+            return () => {
+                document.body.classList.remove('!overflow-hidden');
+            };
+        }
     }, [loading]);
 
-    return <section className='absolute top-0 left-0 z-[1000] bg-white dark:bg-dark w-screen h-screen'>
+    if (!visible) return null;
+
+    return <section className='fixed top-0 left-0 z-[1000] bg-white dark:bg-dark w-screen h-screen'>
         <div className="container flex flex-col justify-center items-center h-full space-y-4">
             <Loading className='w-60' />
             <h2 className='text-3xl'>Loading...</h2>
